719d35c565af17ff69463d8924085d495f9a1692
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-26 21:11:11 +0530

    home page increment button and result widget test

--- normal_app_widget_test/test/home_page_test.dart
+++ normal_app_widget_test/test/home_page_test.dart
@@ -10,18 +10,25 @@ void main() {
   testWidgets(
       'given couter is 0, when increment button is clicked, then counter should be 1',
       (tester) async {
-    await tester.pumpWidget(
-      const MaterialApp(
-        home: MyHomePage(
-          title: 'Counter App',
-        ),
-      ),
-    );
+    await tester.pumpWidget(const MaterialApp(
+      home: MyHomePage(title: 'COunterapp'),
+    ));
 
     final ctr = find.text('0');
     expect(ctr, findsOneWidget);
 
-    final ctr2 = find.text('2');
+    final ctr2 = find.text('1');
     expect(ctr2, findsNothing);
+
+    final incrematBtn = find.byType(FloatingActionButton);
+    await tester.tap(incrematBtn);
+
+    await tester.pump();
+
+    final ctr3 = find.text('1');
+    expect(ctr3, findsOneWidget);
+
+    final ctr4 = find.text('0');
+    expect(ctr4, findsNothing);
   });
 }
