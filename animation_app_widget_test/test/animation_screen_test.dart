efeb7cc6ee727376af34e577db243197da15f8ac
Kasun Jayawardhana <kasunpradeep.ds@gmail.com>
2024-11-29 10:34:31 +0530

    AFTER ANIMATION TEST CASE in animation screen

--- animation_app_widget_test/test/animation_screen_test.dart
+++ animation_app_widget_test/test/animation_screen_test.dart
@@ -6,15 +6,25 @@ void main() {
   testWidgets('animation screen test', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: AnimationScreen()));
 
-    final containerFinder = find.byType(Container);
+    var containerFinder = find.byType(Container);
     expect(containerFinder, findsOneWidget);
-    final container =
-        tester.widget<Container>(containerFinder); // type container
+    var container = tester.widget<Container>(containerFinder); // type container
     // INITIAL TEST CASES
     expect(container.constraints!.maxWidth, 50);
     expect(container.constraints!.maxHeight, 50);
     expect((container.decoration as BoxDecoration).borderRadius,
         BorderRadius.zero);
     expect((container.decoration as BoxDecoration).color, Colors.blue);
+
+    // ANIMATION TEST CASES
+    await tester.pumpAndSettle(); // wait for animation to complete
+
+    containerFinder = find.byType(Container);
+    container = tester.widget<Container>(containerFinder);
+    expect(container.constraints!.maxHeight, 200);
+    expect(container.constraints!.maxWidth, 200);
+    expect((container.decoration as BoxDecoration).borderRadius,
+        BorderRadius.circular(50));
+    expect((container.decoration as BoxDecoration).color, Colors.green);
   });
 }
